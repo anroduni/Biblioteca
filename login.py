1ae3ad452f873e8bec7824a1c301efca4b59b5ed
orcahe <149510274+orcahe@users.noreply.github.com>
2024-07-11 16:35:15 -0600

    Nuevo Login, tabla en CUC

--- login.py
+++ login.py
@@ -6,10 +6,16 @@ from tkinter import messagebox
 import sqlite3
 import subprocess
 import sys
+from PIL import Image, ImageTk, ImageDraw
 
+def create_rounded_rectangle_image(width, height, radius, color):
+    image = Image.new('RGBA', (width, height), (0, 0, 0, 0))
+    draw = ImageDraw.Draw(image)
+    draw.rounded_rectangle((0, 0, width, height), radius=radius, fill=color)
+    return image
 
 def login(frame_principal, usuario, contrasena):
-    conn = sqlite3.connect('usuarios.db')
+    conn = sqlite3.connect('BibliotecaCUC/bibliotecaCelsus.db')
     cursor = conn.cursor()
 
     cursor.execute('''
@@ -35,22 +41,29 @@ def limpiar(frame):
 # Función para crear la pantalla de login
 def crear_pantalla_login(frame_principal):
     limpiar(frame_principal)
+
+    imagen = Image.open("logo.png")
+    imagen = imagen.resize((150, 200))
+    img = ImageTk.PhotoImage(imagen)
+    lbl_img = tk.Label(frame_principal, image=img, bg="white")
+    lbl_img.image = img  # keep a reference to prevent garbage collection
+    lbl_img.pack(pady=10)
     
-    tk.Label(frame_principal, text="Usuario:").pack(pady=5)
+    tk.Label(frame_principal,bg="white", text="Usuario:").pack(pady=0)
     usuario_entry = tk.Entry(frame_principal)
     usuario_entry.pack(pady=5)
 
-    tk.Label(frame_principal, text="Contraseña:").pack(pady=5)
+    tk.Label(frame_principal, bg="white", text="Contraseña:").pack(pady=5)
     contrasena_entry = tk.Entry(frame_principal, show="*")
     contrasena_entry.pack(pady=5)
 
-    login_button = tk.Button(frame_principal, text="Login", command=lambda: login(frame_principal, usuario_entry.get(), contrasena_entry.get()))
-    login_button.pack(pady=20)
+    login_button = tk.Button(frame_principal, text="Login",pady=5, padx=50, bg="black",fg="white",relief="flat", command=lambda: login(frame_principal, usuario_entry.get(), contrasena_entry.get()))
+    login_button.pack(pady=10)
 
 # Crear la ventana principal
 root = tk.Tk()
 root.title("Sistema de Login")
-root.geometry("300x200")
+root.geometry("300x450")
 
 frame_principal = tk.Frame(root, bg="white")
 frame_principal.pack(expand=True, fill='both')
