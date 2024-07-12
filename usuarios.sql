1f49a0935dd5645bbc5262b44adf3c4a81576d00
anroduni <antonio.rodriguez@unicuc.mx>
2024-07-12 02:41:40 -0600

    Eliminacion de bases de datos obsoletas

--- usuarios.sql
+++
@@ -1,3 +0,0 @@
-Create Table usuarios (usuario text not null, contrasena text not null)
-insert into usuarios(usuario, contrasena) values ("kevin", "12345")
---DELETE  from usuarios where usuario = "omar"
